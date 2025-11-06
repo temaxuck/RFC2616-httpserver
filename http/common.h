ecaa0a0cf3d91666b7a6be423136b26fab33d5d4
temaxuck <gorropand@gmail.com>
2025-11-06 13:21:54 +0300

    WIP: Introduce http_reason_phrase()

--- http/common.h
+++ http/common.h
@@ -1,6 +1,7 @@
 #ifndef HTTP_COMMON_H
 #  define HTTP_COMMON_H
 
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -78,53 +79,60 @@ char *http_method_to_cstr(HTTP_Method method) {
 
 // NOTE: For now, only default statuses (as specified by RFC 2616) are allowed
 #define HTTP_STATUS_MAP(XX)                                             \
-    XX(0,   UNKNOWN,                          Unrecognized Status)       \
-    XX(100, CONTINUE,                        Continue)                  \
-    XX(101, SWITCHING_PROTOCOLS,             Switching Protocols)       \
-    XX(200, OK,                              OK)                        \
-    XX(201, CREATED,                         Created)                   \
-    XX(202, ACCEPTED,                        Accepted)                  \
-    XX(203, NON_AUTHORITATIVE_INFORMATION,   Non-Authoritative Information) \
-    XX(204, NO_CONTENT,                      No Content)                \
-    XX(205, RESET_CONTENT,                   Reset Content)             \
-    XX(206, PARTIAL_CONTENT,                 Partial Content)           \
-    XX(300, MULTIPLE_CHOICES,                Multiple Choices)          \
-    XX(301, MOVED_PERMANENTLY,               Moved Permanently)         \
-    XX(302, FOUND,                           Found)                     \
-    XX(303, SEE_OTHER,                       See Other)                 \
-    XX(304, NOT_MODIFIED,                    Not Modified)              \
-    XX(305, USE_PROXY,                       Use Proxy)                 \
-    XX(307, TEMPORARY_REDIRECT,              Temporary Redirect)        \
-    XX(400, BAD_REQUEST,                     Bad Request)               \
-    XX(401, UNAUTHORIZED,                    Unauthorized)              \
-    XX(402, PAYMENT_REQUIRED,                Payment Required)          \
-    XX(403, FORBIDDEN,                       Forbidden)                 \
-    XX(404, NOT_FOUND,                       Not Found)                 \
-    XX(405, METHOD_NOT_ALLOWED,              Method Not Allowed)        \
-    XX(406, NOT_ACCEPTABLE,                  Not Acceptable)            \
-    XX(407, PROXY_AUTHENTICATION_REQUIRED,   Proxy Authentication Required) \
-    XX(408, REQUEST_TIMEOUT,                 Request Timeout)           \
-    XX(409, CONFLICT,                        Conflict)                  \
-    XX(410, GONE,                            Gone)                      \
-    XX(411, LENGTH_REQUIRED,                 Length Required)           \
-    XX(412, PRECONDITION_FAILED,             Precondition Failed)       \
-    XX(413, PAYLOAD_TOO_LARGE,               Payload Too Large)         \
-    XX(414, URI_TOO_LONG,                    URI Too Long)              \
-    XX(415, UNSUPPORTED_MEDIA_TYPE,          Unsupported Media Type)    \
-    XX(416, RANGE_NOT_SATISFIABLE,           Range Not Satisfiable)     \
-    XX(417, EXPECTATION_FAILED,              Expectation Failed)        \
-    XX(500, INTERNAL_SERVER_ERROR,           Internal Server Error)     \
-    XX(501, NOT_IMPLEMENTED,                 Not Implemented)           \
-    XX(502, BAD_GATEWAY,                     Bad Gateway)               \
-    XX(503, SERVICE_UNAVAILABLE,             Service Unavailable)       \
-    XX(504, GATEWAY_TIMEOUT,                 Gateway Timeout)           \
-    XX(505, HTTP_VERSION_NOT_SUPPORTED,      HTTP Version Not Supported)
+    XX(0,   UNKNOWN,                         "Unrecognized Status")     \
+    XX(100, CONTINUE,                        "Continue")                \
+    XX(101, SWITCHING_PROTOCOLS,             "Switching Protocols")     \
+    XX(200, OK,                              "OK")                      \
+    XX(201, CREATED,                         "Created")                 \
+    XX(202, ACCEPTED,                        "Accepted")                \
+    XX(203, NON_AUTHORITATIVE_INFORMATION,   "Non-Authoritative Information") \
+    XX(204, NO_CONTENT,                      "No Content")              \
+    XX(205, RESET_CONTENT,                   "Reset Content")           \
+    XX(206, PARTIAL_CONTENT,                 "Partial Content")         \
+    XX(300, MULTIPLE_CHOICES,                "Multiple Choices")        \
+    XX(301, MOVED_PERMANENTLY,               "Moved Permanently")       \
+    XX(302, FOUND,                           "Found")                   \
+    XX(303, SEE_OTHER,                       "See Other")               \
+    XX(304, NOT_MODIFIED,                    "Not Modified")            \
+    XX(305, USE_PROXY,                       "Use Proxy")               \
+    XX(307, TEMPORARY_REDIRECT,              "Temporary Redirect")      \
+    XX(400, BAD_REQUEST,                     "Bad Request")             \
+    XX(401, UNAUTHORIZED,                    "Unauthorized")            \
+    XX(402, PAYMENT_REQUIRED,                "Payment Required")        \
+    XX(403, FORBIDDEN,                       "Forbidden")               \
+    XX(404, NOT_FOUND,                       "Not Found")               \
+    XX(405, METHOD_NOT_ALLOWED,              "Method Not Allowed")      \
+    XX(406, NOT_ACCEPTABLE,                  "Not Acceptable")          \
+    XX(407, PROXY_AUTHENTICATION_REQUIRED,   "Proxy Authentication Required") \
+    XX(408, REQUEST_TIMEOUT,                 "Request Timeout")         \
+    XX(409, CONFLICT,                        "Conflict")                \
+    XX(410, GONE,                            "Gone")                    \
+    XX(411, LENGTH_REQUIRED,                 "Length Required")         \
+    XX(412, PRECONDITION_FAILED,             "Precondition Failed")     \
+    XX(413, PAYLOAD_TOO_LARGE,               "Payload Too Large")       \
+    XX(414, URI_TOO_LONG,                    "URI Too Long")            \
+    XX(415, UNSUPPORTED_MEDIA_TYPE,          "Unsupported Media Type")  \
+    XX(416, RANGE_NOT_SATISFIABLE,           "Range Not Satisfiable")   \
+    XX(417, EXPECTATION_FAILED,              "Expectation Failed")      \
+    XX(500, INTERNAL_SERVER_ERROR,           "Internal Server Error")   \
+    XX(501, NOT_IMPLEMENTED,                 "Not Implemented")         \
+    XX(502, BAD_GATEWAY,                     "Bad Gateway")             \
+    XX(503, SERVICE_UNAVAILABLE,             "Service Unavailable")     \
+    XX(504, GATEWAY_TIMEOUT,                 "Gateway Timeout")         \
+    XX(505, HTTP_VERSION_NOT_SUPPORTED,      "HTTP Version Not Supported")
 
 typedef enum {
 #define XX(num, name, ...) HTTP_Status_##name = num,
     HTTP_STATUS_MAP(XX)
 #undef XX
 } HTTP_Status;
+
+char *http_reason_phrase(uint16_t status_code) {
+#define XX(num, name, phrase) if (status_code == num) return phrase;
+    HTTP_STATUS_MAP(XX)
+#undef XX
+    return "UNKNOWN";
+}
 
 /**
  * Frees header `h`.
